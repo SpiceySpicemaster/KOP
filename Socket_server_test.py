39e6524983fbc3264595c984f742a3db5c61c40a
Gergő <galffygergo@gmail.com>
2023-01-30 03:27:34 +0100

    Final cucc

--- Socket_server_test.py
+++ Socket_server_test.py
@@ -65,7 +65,6 @@ def return_user(addr):
         return list(logged_in.keys())[c]
     except IndexError or ValueError:
         return False
-
 def is_admin(usr):
     # If there are no admins yet it always returns True!
 
